b675150b99dd3c6484dd0e29092f24daa6bfb89b
kanekyo1234 <kanekyohunter.0314@softbank.ne.jp>
2021-05-17 15:53:35 +0900

    (>_<)

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const getMovies = require("./getMovies");
 const getMoviesDetails = require("./getMoviesDetails");
 const getMoviesCredits = require("./getMoviesCredits");
 
-const FROM = 2009;
-const TO = 2021;
+const FROM = 2014;
+const TO = 2016;
 
 const writeJSON = (path, data) => {
   fs.writeFileSync(path, JSON.stringify(data));
