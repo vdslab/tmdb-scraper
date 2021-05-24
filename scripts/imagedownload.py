585a435f7d4b645288a727dd010d15ded6711e4d
kanekyo1234 <kanekyohunter.0314@softbank.ne.jp>
2021-05-24 12:30:06 +0900

    add revenue_image

--- scripts/imagedownload.py
+++ scripts/imagedownload.py
@@ -6,11 +6,12 @@ API_ENDPOINT = 'https://image.tmdb.org/t/p/w500'
 with open('../data/csv/JP_DETAILS_REVENUE_FILTER.csv', encoding='utf_8_sig', mode='r') as f:
     dataReader = csv.reader(f)
     for row in dataReader:
-        if number == 0:  # ここは最初にposter_pathの題名が入っているせいで書いためんどいから汚い条件式だけど言ったゆるして☺
+        # ここは最初にposter_pathの題名が入っているせいで書いためんどいから汚い条件式だけど言ったゆるして☺
+        if number <= 1473 or row[12] == "":
             number += 1
             continue
+        
         URL2 = API_ENDPOINT+row[12]  # 12番目に
         URL = f"https://image.tmdb.org/t/p/w500/{row[12]}"
-        if number <= 100:  # ここの値変えて枚数を確定
-            request.urlretrieve(URL2, f"../data/image/image{number}.jpg")
+        request.urlretrieve(URL2, f"../data/image/image{number}.jpg")
         number += 1
